0f764405353b889eeef904a154fda599f9a76b35
Joshua Turner <joshua.turner2021@gmail.com>
2022-08-03 23:22:47 +1000

    Add project files.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
