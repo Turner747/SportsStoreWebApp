ca9683541818848f98899f746c688db7b5a8fb8f
Joshua Turner <joshua.turner2021@gmail.com>
2022-09-02 22:09:03 +1000

    chapter 9 complete

---
+++ SportsStore/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [BindNever] 
+        public int OrderID { get; set; }
+
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter an address (starting in the first line")]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a state")]
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter a country name")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
